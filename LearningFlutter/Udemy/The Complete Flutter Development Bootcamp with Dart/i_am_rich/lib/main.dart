cb2640e1e584d7b122a24611755e1d1f290fec3c
thanhtungpfiev <thanhtungpfiev@gmail.com>
2023-08-11 10:03:04 +0700

    Udemy I Am Rich App Completed

---
+++ LearningFlutter/Udemy/The Complete Flutter Development Bootcamp with Dart/i_am_rich/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.asset(
+            'assets/images/diamond.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
