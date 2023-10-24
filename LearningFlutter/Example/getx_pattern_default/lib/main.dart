73c2ad48073c42d2c6f94a7e72a3c66a34c07e44
thanhtungpfiev <thanhtungpfiev@gmail.com>
2023-10-24 20:50:00 +0700

    Structure for small and medium project using getx v2

--- LearningFlutter/Example/getx_pattern_default/lib/main.dart
+++ LearningFlutter/Example/getx_pattern_default/lib/main.dart
@@ -5,11 +5,25 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
-    GetMaterialApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
