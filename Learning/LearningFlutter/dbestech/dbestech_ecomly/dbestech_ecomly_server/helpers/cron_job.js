4e0eb2e7a14263df1c1219febf75c948b9617846
Dao Thanh Tung (MS/EAE1-BST) <tung.daothanh@vn.bosch.com>
2025-10-29 10:56:57 +0700

    dbestech ecomly server cron job for cart product

--- Learning/LearningFlutter/dbestech/dbestech_ecomly/dbestech_ecomly_server/helpers/cron_job.js
+++ Learning/LearningFlutter/dbestech/dbestech_ecomly/dbestech_ecomly_server/helpers/cron_job.js
@@ -16,6 +16,8 @@
 const cron = require("node-cron");
 const { Category } = require("../models/category");
 const { Product } = require("../models/product");
+const { CartProduct } = require("../models/cart_product");
+const { default: mongoose } = require("mongoose");
 
 cron.schedule("0 0 * * *", async () => {
   console.log("Running a job at midnight every day");
@@ -35,5 +37,54 @@ cron.schedule("0 0 * * *", async () => {
     console.log("Scheduled task completed successfully.");
   } catch (error) {
     console.error("Error executing scheduled task:", error);
+  }
+});
+
+cron.schedule("*/30 * * * *", async () => {
+  console.log("Running a job every 30 minutes");
+  // Add your scheduled task logic here
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    console.log("Reservation release CRON job started at", new Date());
+    const expiredReservations = await CartProduct.find({
+      reserved: true,
+      reservationExpiry: { $lte: new Date() },
+    }).session(session);
+    for (const cartProduct of expiredReservations) {
+      const product = await Product.findById(cartProduct.product).session(
+        session
+      );
+      if (product) {
+        const updatedProduct = await Product.findByIdAndUpdate(
+          product._id,
+          { $inc: { countInStock: cartProduct.quantity } },
+          { new: true, runValidators: true, session }
+        );
+        if (!updatedProduct) {
+          console.error(
+            "Error occurred: Product update failed. Potential concurrency issue."
+          );
+          await session.abortTransaction();
+          return;
+        }
+
+        await CartProduct.findByIdAndUpdate(
+          cartProduct._id,
+          { reserved: false },
+          { session }
+        );
+      }
+      await session.commitTransaction();
+      console.log("Reservation release CRON job completed at", new Date());
+    }
+  } catch (error) {
+    await session.abortTransaction();
+    console.error("Error executing scheduled task:", error);
+    return resizeBy
+      .status(500)
+      .json({ type: error.name, message: error.message });
+  } finally {
+    session.endSession();
   }
 });
